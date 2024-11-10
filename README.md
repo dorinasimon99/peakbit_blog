35965dbe9ed3431e0e7a7a8a3d4696ef9cde147b
Dorina Simon <dorina.simon99@gmail.com>
2024-11-10 09:35:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# peakbit_blog
+
+A Flutter trial project for PeakBit.
+
+## Time tracking
+
+2024.11.10 9:20 - 9:35 Setup Flutter project and GitHub
---
+++ android/app/src/main/kotlin/com/peakbit/trial/peakbit_blog/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.peakbit.trial.peakbit_blog
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
